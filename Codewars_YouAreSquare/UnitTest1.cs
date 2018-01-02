e1ca1ee2490fed87cbee48c70856bc3361f2e49e
sqz777In91 <sqz777isme@gmail.com>
2018-01-02 16:00:16 +0800

    加入專案檔案。

---
+++ Codewars_YouAreSquare/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_YouAreSquare
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
